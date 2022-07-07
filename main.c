81e9cb8a7a99eb214ecb35aaa3f87ae76ed1769f
Thomas Dokas <dokastho@umich.edu>
2022-07-07 23:32:02 +0000

    add template socket code

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "parrot.h"
+#include <pthread.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <stdio.h>
 
 // global variable declarations
 char term_cache[64][64];
@@ -16,5 +19,66 @@ int main() {
         return 1;
     }
     chirp();
+    int port = 0;
+
+    struct sockaddr_in addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    // (1) Create socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        perror("Error opening stream socket");
+        // exit(1);
+        return -1;
+    }
+
+    // (2) Set the "reuse port" socket option
+    int yesval = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yesval, sizeof(yesval)) == -1)
+    {
+        perror("Error setting socket options");
+        // exit(1);
+        return -1;
+    }
+    // (3) Bind to the port.
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        perror("Error binding stream socket");
+        // exit(1);
+        return -1;
+    }
+
+    // (3b) Detect which port was chosen.
+    port = get_port_number(sock);
+
+    listen(sock, 30);
+
+    // initialize tag keywords
+    if (init())
+    {
+        // error in connecting to other parrots
+        return -1;
+    }
+
+    printf("@@@ port %d", port);
+    while (1)
+    {
+        int clientsocket = accept(sock, 0, 0);
+        if (clientsocket == -1)
+        {
+            perror("Error accepting connection");
+            // exit(1);
+            return -1;
+        }
+
+        // threading comes later...
+        // std::thread s(handle_connection, clientsocket);
+        // s.detach();
+    }
     pthread_mutex_destroy(&sock_lock);
 }
--- parrot.c
+++ parrot.c
@@ -1,6 +1,43 @@
 #include "parrot.h"
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
+// connection functions
+
+// send len bytes from buf over sock
+void send_bytes(int sock, const char *buf, int len)
+{
+    int sent = 0;
+    int n = 0;
+    do
+    {
+        n = send(sock, buf + sent, len - sent, MSG_NOSIGNAL);
+        sent += n;
+    } while (sent < len);
+}
+
+int get_port_number(int sockfd)
+{
+    struct sockaddr_in addr;
+    socklen_t length = sizeof(addr);
+    if (getsockname(sockfd, (struct sockaddr *)&addr, &length) == -1)
+    {
+        perror("Error getting port of socket");
+        // exit(1);
+        return -1;
+    }
+    // Use ntohs to convert from network byte order to host byte order.
+    return ntohs(addr.sin_port);
+}
+
+// utility functions
+
+
+// purposeful functions
 int
 chirp()
 {
